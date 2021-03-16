f759975b1e399bd8aca2d8d98d0b03c58248d73d
Angelo Lebon <angelo.lebon@howest.be>
2021-03-16 08:28:17 +0100

    Lesvoorbeeld Forms

---
+++ FormViewModelExample/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using FormViewModelExample.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormViewModelExample.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        [Route("/login")]
+        public IActionResult Login()
+        {
+            AccountLoginVm vm = new AccountLoginVm();
+            vm.Email = "Test";
+            vm.Password = "123";
+            return View(vm);
+        }
+
+        [HttpPost]
+        [Route("/login")]
+        public IActionResult Login(AccountLoginVm accountLoginVm)
+        {
+            return RedirectToAction("contact", "home");
+        }
+
+        [HttpGet]
+        [Route("/admin/home")]
+        public IActionResult AdminHomepage()
+        {
+            return Content("You are logged in!");
+        }
+
+
+    }
+}
---
+++ FormViewModelExample/ViewModels/AccountLoginVm.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormViewModelExample.ViewModels
+{
+    public class AccountLoginVm
+    {
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
